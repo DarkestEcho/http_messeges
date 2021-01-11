0cd7b3491b418a5b852da91dc51a9ed47f8ae48a
Echo <Samarin99@gmail.com>
2021-01-11 19:04:59 +0200

    v1.0

---
+++ MessageStore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MessageStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Message> _messages = new List<Message>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+                
+        public IActionResult Send(Message message)
+        {
+            if (message != null && !string.IsNullOrEmpty(message?.Text))
+            {
+                message.Date = DateTime.Now;
+                _messages.Add(message);
+            }
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {            
+            return View(_messages.OrderByDescending(_ => _.Date).Take(5).ToList());
+        }
+    }
+}
---
+++ MessageStore/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageStore.Models
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
